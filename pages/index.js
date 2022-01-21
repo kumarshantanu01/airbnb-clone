8607bfb5a0498955a434e32ca3f6a5c2f8b00af3
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-21 17:28:50 +0530

    feat: small medium & large cards added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-export default function Home({exploreData}) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -19,10 +22,45 @@ export default function Home({exploreData}) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {exploreData?.map((item) =>(
-            <h1>{item.location}</h1>
-          ))}
+
+          {/* Pulling data from server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, location, distance }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
         </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+
+          {/* OR */}
+          {/* {cardsData?.map(item =>(
+            <MediumCard 
+              key={item.img}
+              img={item.img}
+              title={item.title}
+            />
+          ))} */}
+        </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlist curated by Airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   );
@@ -34,9 +72,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
-  return{
-    props:{
-      exploreData
-    }
-  }
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
