c7055a69884469ac255ac1612ebc433e9496587b
kumarshantanu01 <shantanuk280@gmail.com>
2022-01-22 16:02:33 +0530

    feat: adding calendar with date range picker

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import {  SearchIcon,
-    GlobeAltIcon,
-    MenuIcon,
-    UserCircleIcon,
-    UsersIcon, } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+} from "@heroicons/react/solid";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
+  {
+    /* INSERTING CALENDAR */
+  }
+  const [searchInput, setsearchInput] = useState("");
+  const [startDate, setStartdate] = useState(new Date());
+  const [endDate, setEnddate] = useState(new Date());
+
+  const handleSelect = (ranges) => {
+    setStartdate(ranges.selection.startDate);
+    setEnddate(ranges.selection.endDate);
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left side header */}
@@ -25,6 +48,9 @@ function Header() {
 
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          // VALUE AND ONCHANGE FOR CALENDAR
+          value={searchInput}
+          onChange={(e) => setsearchInput(e.target.value)}
           className="pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400  flex-grow"
           type="text"
           placeholder="Start your search"
@@ -35,15 +61,31 @@ function Header() {
       {/* right header */}
 
       <div className="flex space-x-4 items-center justify-end text-gray-500">
-      {/* space-x-4 means every child element will have padding 4 */}
-          <p className="hidden md:inline cursor-pointer">Become a host</p>
-          <GlobeAltIcon className="h-6 cursor-pointer"/>
+        {/* space-x-4 means every child element will have padding 4 */}
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
 
-          <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-              <MenuIcon className="h-6"/>
-              <UserCircleIcon className="h-6"/>
-          </div>
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
       </div>
+
+      {/* when using this, as soon as we will enter text in search calendar will appear */}
+      {/* example below */}
+      {/* {searchInput && <h1>sfdgrd</h1>} */}
+
+      {/* INSERTING CALENDAR */}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-2">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+        </div>
+      )}
     </header>
   );
 }
